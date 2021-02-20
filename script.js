71d388b5b9f0ce255907888e925f00c318a45b9c
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-02-20 23:10:43 +0600

    spinner added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById('generate-btn')
     })
 
 const generateQuote = () =>{
+    toggleSpinner(true);
     fetch(`https://api.quotable.io/random`)
     .then(res => res.json())
     .then(data => showQuote(data))
@@ -13,5 +14,16 @@ const generateQuote = () =>{
 const showQuote = (data) =>{
     document.getElementById('speech').innerText = data.content;
     document.getElementById('author').innerText = data.author;
-
+    toggleSpinner(false);
 }
+
+const toggleSpinner = (show) => {
+    const spinner = document.getElementById("loading-spinner");
+    if(show){
+      spinner.classList.remove("d-none");
+    }
+    else{
+      spinner.classList.add("d-none");
+    }
+      
+  }
